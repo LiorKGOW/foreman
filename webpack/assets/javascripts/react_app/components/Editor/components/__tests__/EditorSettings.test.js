a4a8ca5db2e7ec7e46605b7d14039d2150902ff2
Lior Keren <lkeren@redhat.com>
2023-06-05 15:11:38 +0300

    Fixes #36106 - EditorNavbar PF4 Refactor

--- webpack/assets/javascripts/react_app/components/Editor/components/__tests__/EditorSettings.test.js
+++ webpack/assets/javascripts/react_app/components/Editor/components/__tests__/EditorSettings.test.js
@@ -1,31 +1,87 @@
-import { testComponentSnapshotsWithFixtures } from '../../../../common/testHelpers';
-import { noop } from '../../../../common/helpers';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 
 import EditorSettings from '../EditorSettings';
-import { dropdowns } from '../../Editor.fixtures';
+import {
+  inputEditorContextValue,
+  previewEditorContextValue,
+} from '../../Editor.fixtures';
+import { EditorContext } from '../../EditorContext';
+import { editorSettingsFixtures } from '../../Editor.fixtures';
 
-const fixtures = {
-  'renders EditorSettings': {
-    ...dropdowns,
-    mode: 'Ruby',
-    selectedView: 'input',
-    theme: 'Github',
-    autocompletion: true,
-    liveAutocompletion: false,
-    keyBinding: 'vim',
-    changeSetting: noop,
-  },
-  'renders EditorSettings w/preview': {
-    ...dropdowns,
-    mode: 'Ruby',
-    selectedView: 'preview',
-    theme: 'Github',
-    autocompletion: true,
-    liveAutocompletion: false,
-    keyBinding: 'vim',
-    changeSetting: noop,
-  },
-};
+describe('EditorSettings', () => {
+  it('should render all elements in preview view', async () => {
+    const { getByText, getByLabelText, getAllByLabelText } = await render(
+      <EditorContext.Provider value={previewEditorContextValue}>
+        <EditorSettings {...editorSettingsFixtures} />
+      </EditorContext.Provider>
+    );
 
-describe('EditorSettings', () =>
-  testComponentSnapshotsWithFixtures(EditorSettings, fixtures));
+    const editorSettingsOpenbutton = getAllByLabelText(/EditorSettings button/i)[1];
+    
+    // Open EdtiorSettings
+    fireEvent.click(editorSettingsOpenbutton);
+
+    // another try:
+    // const editorSettingsOpenbuttoncontainer = getByLabelText(/EditorSettings button container/i);
+    // const editorSettingsOpenbutton2 = editorSettingsOpenbuttoncontainer.children[0].children[0];
+    // fireEvent.click(editorSettingsOpenbutton2);
+
+
+    // // Titles
+    // expect(getByText(/Settings/i)).toBeInTheDocument();
+    // expect(getByText(/Syntax/i)).toBeInTheDocument();
+    // expect(getByText(/Keybind/i)).toBeInTheDocument();
+    // expect(getByText(/Theme/i)).toBeInTheDocument();
+    // expect(getByText(/Autocompletion/i)).toBeInTheDocument();
+    // expect(getByText(/Live Autocompletion/i)).toBeInTheDocument();
+
+    // DropDowns
+    const modeDropdown = getByLabelText(/Mode Dropdown/i);
+    const keybindingsDropdown = getByLabelText(/Keybindings Dropdown/i);
+    const themesDropdown = getByLabelText(/Themes Dropdown/i);
+
+    // Should be disabled in preview view
+    expect(modeDropdown).toBeDisabled();
+    expect(keybindingsDropdown).toBeDisabled();
+
+    expect(themesDropdown).not.toBeDisabled();
+
+    // autocompletion
+    expect(getByLabelText(/autocompletion input/i).toBeInTheDocument());
+  });
+
+  it('should render mode & keybindings dropdowns in input view as not disabled', () => {
+    const { getByText, getByLabelText } = render(
+      <EditorContext.Provider value={previewEditorContextValue}>
+        <EditorSettings {...editorSettingsFixtures} />
+      </EditorContext.Provider>
+    );
+
+    const modeDropdown = getByLabelText(/Mode Dropdown/i);
+    const keybindingsDropdown = getByLabelText(/Keybindings Dropdown/i);
+
+    // Should be not disabled in input view
+    expect(modeDropdown).not.toBeDisabled();
+    expect(keybindingsDropdown).not.toBeDisabled();
+    });
+});
+
+// describe('EditorSettings', () => {
+//   it('should render with input view', () => {
+//     const wrapper = shallow(
+//       <EditorContext.Provider value={{inputSelectedView}}>
+//         <EditorSettings {...editorSettingsFixtures} />
+//       </EditorContext.Provider>
+//     );
+//     expect(wrapper).toMatchSnapshot();
+//   });
+//   it('should render with preview view', () => {
+//     const wrapper = shallow(
+//       <EditorContext.Provider value={{previewSelectedView}}>
+//         <EditorSettings {...editorSettingsFixtures} />
+//       </EditorContext.Provider>
+//     );
+//     expect(wrapper).toMatchSnapshot();
+//   });
+// });
